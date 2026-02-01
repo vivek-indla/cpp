b08d4416f3798abc8dea60de08a3df8903238997
vivek indla <25215a7213@bvrit.ac.in>
2026-02-01 08:55:33 +0530

    first commit

--- bitmanip/ith-bit-is-set-or-not.cpp
+++ bitmanip/ith-bit-is-set-or-not.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=10, i=1;
-    // cout<<"enter a number and position: ";
-    // cin>>n>>i;
-    int set=n>>i;
-    cout<<set<<endl;
+    int n=15,i=2;
+    int size=0;
+    while(n){
+        size++;
+        n=n>>1;
+    }
+    int set=size-i-1;
     if(set&1){
         cout<<"true";
     }
