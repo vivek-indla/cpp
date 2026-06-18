4d1ee4c50826a9589f1b70bfae61050038076f01
vivek indla <25215a7213@bvrit.ac.in>
2026-06-18 16:44:10 +0530

    changes

--- arrays/remove-duplicates-I.cpp
+++ arrays/remove-duplicates-I.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 class Solution{
     public:
-    int removeDuplicates(vector<int> &nums){
-        if(nums.size()<=1){
-            return nums.size();
+    int removeDuplicates(vector<int>& nums){
+        int n=nums.size();
+        if(n<=1){
+            return n;
         }
         int idx=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[idx]!=nums[i-1]){
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[i-1]){
                 nums[idx++]=nums[i];
-                idx++;
             }
         }
         return idx;
