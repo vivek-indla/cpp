7db57d4586f3e824d16237218ce5307469460f80
vivek indla <25215a7213@bvrit.ac.in>
2026-07-26 03:13:33 +0530

    dd deletion updated

--- linked-list/doubly-linked-list/dllDeletion.cpp
+++ linked-list/doubly-linked-list/dllDeletion.cpp
@@ -33,72 +33,168 @@ void print(Node* head){
         temp=temp->next;
     }
 }
-Node* findTail(Node* head){
+//Deletion of Doubly Linked List
+//Deletion at the head
+Node* DeleteHead(Node* head) {
+    if (head == NULL){
+        return NULL;
+    }
+    if (head->next == NULL) {
+        delete head;
+        return NULL;
+    }
+    Node* temp = head;
+    head=head->next;
+    temp->next=nullptr;
+    head->prev=nullptr;
+    delete temp;
+    return head;
+}
+//Deletion of Tail of the Linkedlist
+Node* DeleteTail(Node* head){
+    if(head==NULL){
+        return head;
+    }
+    if(head->next==NULL){
+        delete head;
+        return NULL;
+    }
     Node* temp=head;
-    Node* tail;
     while(temp){
-        if(temp->next==NULL){
-            tail=temp;
-            return tail;
+        if(temp->next->next==nullptr){
+            Node* newNode=temp->next;
+            newNode->prev=nullptr;
+            temp->next=nullptr;
+            delete newNode;
+            return head;
         }
         temp=temp->next;
     }
     return head;
 }
-void printPrev(Node* tail){
-    Node* temp=tail;
-    while(temp){
-        cout<<temp->data<<" ";
-        temp=temp->prev;
-    }
-}
-//to verify whether the doubly linked list is working better we need cross
-//verify using head traversal and tail traversal
-//Deletion of Doubly Linked List
-//Deletion at the head
-Node* DeleteHead(Node* head){
-    if(head==NULL || head->next==NULL){
+// striver bhai approach
+Node* deleteTail(Node* head){
+    if(head==NULL){
         return head;
     }
-    Node* back=head;
-    head=head->next;
-    head->prev=nullptr;
-    back->next=nullptr;
-    delete back;
+    if(head->next==NULL){
+        delete head;
+        return NULL;
+    }
+    Node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    Node* newNode=temp->prev;
+    newNode->next=nullptr;
+    temp->prev=nullptr;
+    delete temp;
     return head;
 }
-//Deletion of Tail of the Linkedlist
-Node* DeleteTail(Node* head){
-    if(head==NULL||head->next==NULL){
+//it's my approach used to work for every edge case except size of the linked list+1;
+// this will be sorted-out soon :)
+Node* DeleteK(Node* head,int k){
+    if(head==NULL){
+        return NULL;
+    }
+    if(head->next==NULL){
+        delete head;
+        return NULL;
+    }
+    if(k==1){
+        Node* temp=head;
+        head=head->next;
+        temp->next=nullptr;
+        head->prev=nullptr;
+        delete temp;
         return head;
     }
-    Node* back=head;
-    while(back->next->next!=nullptr){
-        if(back->next==NULL){
-            back->next=nullptr;
-            back->next->prev=nullptr;
+    int count=0;
+    Node* temp=head;
+    while(temp){
+        count++;
+        if(count==k-1){
+            Node* newNode=temp->next;
+            temp->next=newNode->next;
+            newNode->prev=temp;
+            newNode->next=nullptr;
+            newNode->prev=nullptr;
+            delete newNode;
             return head;
         }
-        back=back->next;
+        temp=temp->next;
     }
     return head;
 }
-
+//striver bhai approach
+//i have changed to something it the code where the count starts from 1 and if k position only exist in the linkedlist it will continue
+// if further it will directly return head and original linked list printed
+Node* deleteK(Node* head,int k){
+    if(head==NULL){
+        return NULL;
+    }
+    Node* kNode=head;
+    int count=1;
+    while(kNode!=NULL && count<k){
+        count++;
+        kNode=kNode->next;
+    }
+    if(kNode==nullptr){
+        return head;
+    }
+    Node* front=kNode->next;
+    Node* back=kNode->prev;
+    if(back==NULL && front==NULL){
+        return NULL;
+    }
+    else if(back==NULL){
+        return DeleteHead(head);
+    }
+    else if(front==NULL){
+        return deleteTail(head);
+    }
+    back->next=front;
+    front->prev=back;
+    kNode->next=nullptr;
+    kNode->prev=nullptr;
+    delete kNode;
+    return head;
+}
+void DeleteNode(Node* temp){
+    Node* back=temp->prev;
+    Node* front=temp->next;
+    if(front==NULL){
+        back->next=nullptr;
+        temp->prev=nullptr;
+        delete temp;
+        return; 
+    }
+    back->next=front;
+    front->prev=back;
+    temp->next=nullptr;
+    temp->prev=nullptr;
+    delete temp;
+}
 int main(){
-    vector<int> nums={3,2,5,9};
+    vector<int> nums={3,5,2,9};
     Node* head=convert(nums);
-    Node* tail=findTail(head);
     // head=DeleteHead(head);
     // cout<<"head Traversal: ";
     // print(head);
     // cout<<endl;
-    // cout<<"Tail Traversal: ";
-    // printPrev(tail);
     
-    head=DeleteTail(head);
+    // head=DeleteTail(head);
+    // cout<<"head Traversal: ";
+    // print(head);
+    // cout<<endl;
+    
+    // head=deleteK(head,6);
+    // cout<<"head Traversal: ";
+    // print(head);
+    // cout<<endl;
+    
+    DeleteNode(head->next);
     cout<<"head Traversal: ";
     print(head);
     cout<<endl;
-    cout<<"Tail traversal: ";
-    printPrev(tail);
 }
