e36fb7b1238175593b6689480d8aa4f9bbffae47
vivek indla <25215a7213@bvrit.ac.in>
2026-08-21 01:07:48 +0530

    modified

--- linked-list/singly-linked-list/palindromeLL.cpp
+++ linked-list/singly-linked-list/palindromeLL.cpp
@@ -24,23 +24,57 @@ void print(ListNode* head){
         temp=temp->next;
     }
 }
+ ListNode* reverseList(ListNode* head){
+    ListNode* prev=NULL;
+    ListNode* curr=head;
+    while(curr){
+        ListNode* temp=curr->next;
+        curr->next=temp;
+        prev=curr;
+        curr=temp;
+    }
+    return prev;
+ }
 bool palindrome(ListNode* head){
-    stack<int> st;
-    ListNode* temp=head;
-    while(temp){
-        st.push(temp->val);
-        temp=temp->next;
-    }
-    temp=head;
-    while(temp){
-        int value=st.top();
-        if(temp->val!=value){
-            return false;
+    // stack<int> st;
+    // ListNode* temp=head;
+    // while(temp){
+    //     st.push(temp->val);
+    //     temp=temp->next;
+    // }
+    // temp=head;
+    // while(temp){
+    //     int value=st.top();
+    //     if(temp->val!=value){
+    //         return false;
+    //     }
+    //     st.pop();
+    //     temp=temp->next;
+    // }
+    // return true;
+    //above is brute force approach
+        if(head==NULL || head->next==NULL){
+            return true;
         }
-        st.pop();
-        temp=temp->next;
-    }
-    return true;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* secondHead=reverseList(slow->next);
+        ListNode* first=head;
+        ListNode* second=secondHead;
+        while(second!=NULL){
+            if(first->val!=second->val){
+                reverseList(secondHead);
+                return false;
+            }
+            first=first->next;
+            second=second->next;
+        }
+        reverseList(secondHead);
+        return true;
 }
 int main(){
     vector<int> nums={9};
