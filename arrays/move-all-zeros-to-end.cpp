ceb5efb32034b6fc49fe0c664530222c452be21b
vivek indla <25215a7213@bvrit.ac.in>
2026-09-13 10:24:10 +0530

    moveZeroes

--- arrays/move-all-zeros-to-end.cpp
+++ arrays/move-all-zeros-to-end.cpp
@@ -1,31 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution{
-    public:
-    void moveZerosToEnd(vector<int>& nums){
-        int n=nums.size();
-        int x=0;
-        int y=0;
-        while(x<=n && y<=n){
-            if(nums[y]==0){
-                x++;
-                if(nums[x]!=0){
-                    swap(nums[x],nums[y]);
-                    y++;
-                }
-                continue;
+class Solution {
+public:
+    void moveZeros(vector<int>& nums) {
+       int left=0,right=0;
+       while(right<nums.size()){
+            if(nums[right]!=0){
+                swap(nums[right],nums[left]);
+                left++;
             }
-            x++;
-            y++;
-        }
-        for(int i:nums){
-            cout<<i<<" ";
-        }
+            right++;
+       } 
     }
 };
 int main(){
-    Solution s1;    
-    vector<int> nums={1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
-    s1.moveZerosToEnd(nums);
-    return 0;
+    Solution s1;
+    vector<int> nums={1,0,0,3,12};
+    s1.moveZeros(nums);
+    for(int i:nums){
+        cout<<i<<" ";
+    }
 }
